b6818e451ea84d441c98484925f0528ca5f00489
Lucas Hazardous <70599201+LucasHazardous@users.noreply.github.com>
2023-03-17 23:23:25 +0100

    app colors

--- app/src/main/java/com/github/lucashazardous/cvrdlist/ui/theme/Color.kt
+++ app/src/main/java/com/github/lucashazardous/cvrdlist/ui/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Black = Color(0xFF4D455D)
+val Red = Color(0xFFE96479)
+val Beige = Color(0xFFF5E9CF)
+val Teal = Color(0XFF7DB9B6)
