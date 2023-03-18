cdafb124d24d46ae841520c3db8a0feb6e3625ed
Lucas Hazardous <70599201+LucasHazardous@users.noreply.github.com>
2023-03-18 15:57:59 +0100

    opening card adder with floating button

--- app/src/main/java/com/github/lucashazardous/cvrdlist/MainActivity.kt
+++ app/src/main/java/com/github/lucashazardous/cvrdlist/MainActivity.kt
@@ -4,11 +4,11 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -16,13 +16,14 @@ import androidx.compose.ui.unit.dp
 import com.github.lucashazardous.cvrdlist.ui.theme.Beige
 import com.github.lucashazardous.cvrdlist.ui.theme.Black
 import com.github.lucashazardous.cvrdlist.ui.theme.CvrdListTheme
+import com.github.lucashazardous.cvrdlist.ui.theme.Red
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CvrdListTheme {
-Surface(
+                Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
@@ -62,8 +63,17 @@ fun CvrdListView() {
             )
         },
         floatingActionButton = {
-
+            FloatingActionButton(
+                onClick = { addCardOpened.value = !addCardOpened.value },
+                containerColor = Beige, contentColor = Red
+            ) {
+                Icon(
+                    Icons.Filled.Add, "Add card",
+                    modifier = Modifier.size(20.dp)
+                )
+            }
         }) { _ ->
+        CardAdder()
         LazyVerticalGrid(
             columns = GridCells.Fixed(3),
             modifier = Modifier
