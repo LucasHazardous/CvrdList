6076a387dddde8465b628d26dedb2f300105a63e
Lucas Hazardous <70599201+LucasHazardous@users.noreply.github.com>
2023-03-28 17:56:09 +0200

    removed redundant SearchCard properties

--- app/src/main/java/com/github/lucashazardous/cvrdlist/SearchCard.kt
+++ app/src/main/java/com/github/lucashazardous/cvrdlist/SearchCard.kt
@@ -11,90 +11,20 @@ fun SearchCard.toCard(newId: Int) = Card(
 )
 
 data class SearchCard (
-    val id: String,
     val name: String,
-    val supertype: String,
-    val subtypes: List<String>,
-    val hp: String,
-    val types: List<String>,
-    val evolvesTo: List<String>,
-    val rules: List<String>,
-    val attacks: List<Attack>,
-    val weaknesses: List<Weakness>,
-    val retreatCost: List<String>,
-    val convertedRetreatCost: Long,
-    val set: Set,
-    val number: String,
-    val artist: String,
-    val rarity: String,
-    val nationalPokedexNumbers: List<Long>,
-    val legalities: Legalities,
     val images: DataImages,
-    val tcgplayer: Tcgplayer,
-    val cardmarket: Cardmarket
-)
-
-data class Attack (
-    val name: String,
-    val cost: List<String>,
-    val convertedEnergyCost: Long,
-    val damage: String,
-    val text: String
-)
-
-data class DataImages (
-    val small: String,
-    val large: String
-)
-
-data class Legalities (
-    val unlimited: String,
-    val expanded: String
+    val cardmarket: Cardmarket,
+    val set: Set
 )
 
 data class Set (
-    val id: String,
-    val name: String,
-    val series: String,
-    val printedTotal: Long,
-    val total: Long,
-    val legalities: Legalities,
-    val ptcgoCode: String,
-    val releaseDate: String,
-    val updatedAt: String,
-    val images: SetImages
+    val name: String
 )
 
-data class SetImages (
-    val symbol: String,
-    val logo: String
-)
-
-data class Tcgplayer (
-    val url: String,
-    val updatedAt: String,
-    val prices: Prices
-)
-
-data class Prices (
-    val holofoil: Holofoil
-)
-
-data class Holofoil (
-    val low: Double,
-    val mid: Double,
-    val high: Double,
-    val market: Double,
-    val directLow: Double
-)
-
-data class Weakness (
-    val type: String,
-    val value: String
+data class DataImages (
+    val small: String
 )
 
 data class Cardmarket (
-    val url: String,
-    val updatedAt: String,
-    val prices: Map<String, Double>
+    val url: String
 )
